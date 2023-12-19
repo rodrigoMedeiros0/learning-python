2bfd9c73591b49a6cc0a673f6e6b659f824f6198
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2023-12-19 11:17:26 -0300

    operadores aritmeticos

--- variaveis.py
+++ variaveis.py
@@ -1,1 +1,25 @@
-nome= 'Rodrigo'
+nome_usuario= 'Rodrigo'
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Rodrigo', 29
+estado = True
+
+# funcao type()
+# print(type(media))
+# print(type(n2))
+# print(type(nome))
+# print(type(idade))
+# print(type(estado))
+# print(type(1+2j))
+
+#Funcao isinstance()
+# a = 10
+# b = 'Sol'
+# print(isinstance(a, int))
+# print(isinstance(b, str))
+# print(isinstance(a, (int, float)))
+
+a = 40
+c = 3 
+r = a * c
+print(r)
