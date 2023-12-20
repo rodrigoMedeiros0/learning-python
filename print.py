8cf654c298d4db800e573882906c0f11380ad412
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2023-12-20 13:00:42 -0300

    funcao print( )

---
+++ print.py
@@ -0,0 +1,41 @@
+#Sintaxe:
+# print(objetos, argumentos)
+
+# mensagem = 'Função print()'
+# print(mensagem)
+# print('Aula de python')
+
+# nome = 'Rodrigo Vinicius'
+# print('Aluno:', nome)
+
+# nome = input('Digite seu nome: ')
+# msg = 'Olá ' + nome + ', bem vindo ao curso de Python!'
+# print(msg)
+
+# print('Imprime a mensagem e muda de linha')
+# print('Imprime a mensagem e permanece na linha.', end='')
+# print(' Continuo na mesma linha.')
+
+# nome = 'Rodrigo'
+# idade = 29 
+# msg_formatada = 'O nome dele é {0} e ele tem {1} anos.'.format(nome, idade)
+# print(msg_formatada)
+
+#melhor formar de executar o print
+# nome = 'Rodrigo'
+# peso = 82.50
+# msg = f'Olá, meu nome é {nome} e eu peso {peso} quilos.'
+# print(msg)
+
+# a = 10
+# b = 5
+# res = f'A soma de {a} com {b} é igual a {a+b}'
+# print(res)
+
+#formatando string
+# valor = 125.849637
+# print(f'O valor é \'{valor:.2f}\'')
+
+nome = 'André'
+idade = 32
+print(f'Nome: {nome} \tidade: {idade}')
