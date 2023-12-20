d9aaeba4284d625b64e025da5abfe57e46f1711d
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2023-12-19 22:44:01 -0300

    operadores logicos

---
+++ operadores-logicos.py
@@ -0,0 +1,15 @@
+# idade = 15
+# altura = 1.75
+
+# resultado = (idade >= 18) and (altura >= 1.70)
+# msg = 'Pode participar do resultado? ' + str(resultado)
+# input(msg)
+
+# maria = 17
+# joao = 9
+# r = (maria >= 18) or (joao >= 18)
+# msg = 'Maior de idade? ' + str(r)
+# input(msg)
+
+tem_dinheiro = not False
+input('Tem dinheiro? ' + str(tem_dinheiro))
