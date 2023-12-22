f610039a17432fa21c5fd1191c43677c79580f7f
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2023-12-22 20:26:40 -0300

    biblioteca math

---
+++ matematica.py
@@ -0,0 +1,23 @@
+import math
+
+x = 8
+y = 100
+
+raiz_quadrada = math.sqrt(x)
+print(raiz_quadrada)
+#arredonda o valor p cima
+print(math.ceil(raiz_quadrada))
+#arredonda o valor p baixo
+print(math.floor(raiz_quadrada))
+
+#Funções built in - internas 
+valor =[1,2,4,5,6,8,-1,-11,100]
+# print(max(valor))
+# print(min(valor))
+# a = -5
+# b = 4
+# print(abs(a))
+# print(pow(a, b))
+# c = 2.78945
+# print(round(c, 1))
+
---
+++ strings.py
@@ -0,0 +1,4 @@
+nome = 'Rodrigo'
+letra = nome[0]
+print(letra)
+print(len(nome))
